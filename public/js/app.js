6ccc42e2716a16e2cf4e056ecc3ef450c582adca
mayukh joshi <mayukhjoshi262@gmail.com>
2021-09-01 13:39:24 +0530

    removed console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,4 @@
-console.log('client side javascript file')
-// fetch("http://puzzle.mead.io/puzzle").then((response)=>{
-//     response.json().then(data=>{
-//         console.log(data)
-//     })
-// })
+
 
 const weatherForm = document.querySelector('form')
 const search =document.querySelector('input')
